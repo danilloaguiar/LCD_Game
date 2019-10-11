2aa70d3b39422b8621591c276289a8a655638a43
Danillo <danilloaguiar95@gmail.com>
2019-10-11 13:24:00 -0300

    game

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
+/*
+                                                SDA                    SCL
+      Blue Pill, STM32F103xxxx boards.......... PB7                    PB6 
+*/
+
 #include <Arduino.h>
 
 #include <LiquidCrystal_I2C.h>
+
+#define eixoY PA1
+#define LED PC13
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
@@ -9,6 +17,16 @@ uint8_t toy1[8] = {0b00110,	0b00110,	0b00000,	0b11110,	0b01111,	0b01100,	0b11100
 uint8_t toy2[8] = {0b01100,	0b01100,	0b00000,	0b01111,	0b11110,	0b01100,	0b10010,	0b11011};
 
 uint8_t tjb[8] = {0b10010, 0b10100,	0b11000,	0b10000,	0b11000,	0b10101,	0b10011,	0b10111};
+
+boolean position = false;
+boolean toy = false;
+
+ class transistor {
+  public:
+    void andar() {
+      digitalWrite(Pino, HIGH);
+    }
+};
 
 void initGame(){
   
@@ -33,10 +51,43 @@ void initGame(){
   }
 }
 
+
+void running(){
+
+  if (position){
+    lcd.setCursor(0,0);
+    if (toy){
+      lcd.write((byte)0);
+      toy = false;
+    }
+    else{
+      lcd.write((byte)1);
+      toy = true;
+    }
+  }
+  else if (!position)  {
+    lcd.setCursor(0,1);
+    if (toy){
+      lcd.write((byte)0);
+      toy = false;
+    }
+    else{
+      lcd.write((byte)1);
+      toy = true;
+    }
+  }
+  delay(350);
+
+}
+
+
 void setup()
 {
   lcd.init();                      // initialize the lcd 
   
+  pinMode(LED, OUTPUT);
+  transistor t0, t1, t2, t3;
+
   lcd.createChar(0, toy1);
   lcd.createChar(1, toy2);
   lcd.createChar(2, tjb);
@@ -53,11 +104,32 @@ void setup()
   lcd.print("By: Danillo Aguiar");
   delay(3000);
   lcd.clear();
-}
 
+  lcd.setCursor(0,1);
+  lcd.write((byte)0);
+}
 
 
 void loop()
 { 
-  initGame();
+  if ((analogRead(eixoY)) > 800){ 
+    lcd.setCursor(0,1);
+    lcd.print(" ");
+    lcd.setCursor(0,0);
+    lcd.write((byte)0);
+    toy = false;
+    position = true;
+  }
+  else if ((analogRead(eixoY)) < 200){
+    lcd.setCursor(0,1);
+    lcd.write((byte)0);
+    toy = false;
+    lcd.setCursor(0,0);
+    lcd.print(" ");
+    position = false;
+  } 
+
+  running();
+  transistor();
+
 }
